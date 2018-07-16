45a16c2c84565a7ac97972fcc700670fc86f37b4
Alelo <cast@casts-macbook-pro.local>
2018-07-16 08:53:39 -0300

    Exercico numero 2

--- prog1/main.m
+++ prog1/main.m
@@ -10,8 +10,15 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int sum;
+        sum = 50+25;
         // insert code here...
         NSLog(@"Hello, World!");
+        NSLog(@"Hellor world 2");
+        NSLog(@"Testing...\n..1\n...2\n....3\nsum: %i", sum);
+        //Exercicio numero 2.
+        NSLog(@"\nEm Objective-C, as letras minusculas tem significado.\nmain é onde a execução do programa começa.\nChaves de abertura e fechamento cercam as instruções de programa em uma rotina.\nTodas as instriuções de programa devem ser terminada por um ponto e vírgula.");
+        
     }
     return 0;
 }
