6e1237a660d8b44d45df640f873f0867d2f1ebc7
Alelo <cast@casts-macbook-pro.local>
2018-07-16 07:55:17 -0300

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Cast Group on 16/07/2018.
+//  Copyright © 2018 br.hoffsilva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
